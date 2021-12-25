fd65df3c2c48898fd96072ba952787f27edcf37b
Proxides <94020472+Proxides@users.noreply.github.com>
2021-12-25 12:35:12 -0500

    Create Sky_Example.lua

---
+++ Sky_Example.lua
@@ -0,0 +1,28 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Proxides/Library/main/Sky.lua", true))()
+
+local main = library.Init("Library") -- library name
+
+local tab = main:Tab("Tab") -- tab name
+
+tab:Textbox({
+    Text = "Textbox",
+    Info = "Type something in",
+    Placeholder = "..."
+    }, function(text)
+    print(text)
+end)
+
+tab:Button({
+    Name = "Button",
+    Info = "Press the button"
+    }, function()
+    print("Hello world!")
+end)
+
+tab:Toggle({
+    Name = "Toggle",
+    Info = "Click to de/activate",
+    Activated = false
+    }, function(State)
+    print(State)
+end)
